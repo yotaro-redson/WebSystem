a31211026a721939715fc3c563739927f7d82aeb
yotaro-redson <yotaropn@gmail.com>
2023-11-01 19:23:15 +0630

    commit

--- projone/routes/web.php
+++ projone/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::post('/register', [UserController::class,'register']);
